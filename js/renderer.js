16b6545d50ebdec20cdb5e4a3fc3e04c71d4725c
unknown <hironit73@gmail.com>
2026-09-18 16:21:16 +0900

    update data

--- js/renderer.js
+++ js/renderer.js
@@ -87,9 +87,9 @@
         ctx.fill();
 
         // ★ 黒の境界線（エッジ）を追加
-        // ctx.strokeStyle = "#ef5b46";
-        ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
-        ctx.lineWidth = 2.0;
+        ctx.globalAlpha = 0.3;
+        ctx.strokeStyle = "#ef5b46";
+        ctx.lineWidth = 2.5;
         ctx.stroke();
 
         ctx.restore();
